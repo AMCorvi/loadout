6ced144bbeb5c79a8fca133a64fdc6892974e367
AMCorvi <am@corvi.com>
2018-11-24 00:47:01 -0500

    - WIP: creating automation too for configuring exported modules automatically

--- chance.js
+++
@@ -1,4 +0,0 @@
-const C = require("chance");
-const chance = new C();
-
-module.exports = chance
--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const r = require("ramda");
 const rx = require("rxjs");
 const chance = require("chance");
 const esm = require("esm");
-const yml = require("yaml")
+const yml = require("yaml");
+const jsf = require("json-schema-faker");
+const mathjs = require("mathjs")
 
 module.exports = {
   rx: rx,
   r: r,
   chance: chance,
-  esm: esm
-  ,yml: yml
+  esm: esm,
+  yml: yml,
+  jsf: jsf,
+  math: mathjs
 };
+

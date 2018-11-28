2af0a3f13a01a5f7ad2557cc953c6416082a3a07
AMCorvi <am@corvi.com>
2018-11-28 07:32:18 -0500

    feat(lib/update_index_file.js): automate creation of index.js export file

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const r = require("ramda");
-const rx = require("rxjs");
-const chance = require("chance");
-const esm = require("esm");
-const yml = require("yaml");
-const jsf = require("json-schema-faker");
-const mathjs = require("mathjs")
+const chance = require('./chance');
+const esm = require('./esm');
+const jsf = require('./jsf');
+const math = require('./math');
+const r = require('./r');
+const rimraf = require('./rimraf');
+const rx = require('./rx');
+const yaml = require('./yaml');
 
-module.exports = {
-  rx: rx,
-  r: r,
-  chance: chance,
-  esm: esm,
-  yml: yml,
-  jsf: jsf,
-  math: mathjs
-};
-
+  module.exports = {
+  "chance": "chance",
+  "esm": "esm",
+  "jsf": "jsf",
+  "math": "math",
+  "r": "r",
+  "rimraf": "rimraf",
+  "rx": "rx",
+  "yaml": "yaml"
+}
+  
